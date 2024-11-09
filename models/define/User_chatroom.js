21454e3ffd0a475817396f20b8f61d5ef8a5fd84
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2024-11-09 23:58:18 +0900

    Refactor: Erase Automatically generated Model files

--- models/define/User_chatroom.js
+++ models/define/User_chatroom.js
@@ -1,27 +1,9 @@
-/*
-create table project_meet.user_chatroom
-(
-    user_chatroom_id int auto_increment
-        primary key,
-    user_id          int                                       null,
-    chatroom_id      int                                       not null,
-    user_status      varchar(30)                               not null,
-    created_at       timestamp(6) default CURRENT_TIMESTAMP(6) not null,
-    updated_at       timestamp(6) default CURRENT_TIMESTAMP(6) not null on update CURRENT_TIMESTAMP(6),
-    constraint fk_user_chatroom_chatroom
-        foreign key (chatroom_id) references project_meet.chatroom (chatroom_id),
-    constraint fk_user_chatroom_user
-        foreign key (user_id) references project_meet.user (user_id)
-);
-
-
-*/
-
-const { DataTypes } = require("sequelize");
+const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../connectToDB");
 
-const User_chatroom = sequelize.define(
-  "user_chatroom",
+class User_chatroom extends Model {}
+
+User_chatroom.init(
   {
     user_chatroom_id: {
       type: DataTypes.INTEGER,
@@ -53,6 +35,8 @@ const User_chatroom = sequelize.define(
     },
   },
   {
+    sequelize,
+    modelName: "User_chatroom",
     tableName: "user_chatroom",
     timestamps: false,
   }
